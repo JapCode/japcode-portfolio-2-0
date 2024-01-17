f81bab36a862fc0d703ff2e35716d4928c5c48a6
JapCode <japcrowley.thetraveler@gmail.com>
2024-01-17 19:26:07 -0400

    add svgs icons to skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,8 +1,38 @@
 const skills = [
   {
     id: 0,
-    name: '',
-    imageURL: '',
+    name: 'Html',
+    iconName: 'html',
+  },
+  {
+    id: 1,
+    name: 'Css',
+    iconName: 'css',
+  },
+  {
+    id: 2,
+    name: 'PostCSS',
+    iconName: 'postCss',
+  },
+  {
+    id: 3,
+    name: 'JavaScript',
+    iconName: 'javascript',
+  },
+  {
+    id: 4,
+    name: 'Typescript',
+    iconName: 'typescript',
+  },
+  {
+    id: 5,
+    name: 'Node.js',
+    iconName: 'node',
+  },
+  {
+    id: 6,
+    name: 'React',
+    iconName: 'react',
   },
 ];
 export default skills;
